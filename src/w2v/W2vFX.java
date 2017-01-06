0ad246ee8d4b1d4086f737d74fa29901440c9bf5
chocolaolantan <chocolaolantan@gmail.com>
2017-01-06 17:39:46 +0900

    実装クラスのクラス名修正

--- src/w2v/W2vFX.java
+++ src/w2v/W2vFX.java
@@ -9,7 +9,7 @@ import w2v.view.fx.FScene;
 import w2v.view.fx.SScene;
 import w2v.model.W2vModel;
 
-public class W2vProject extends Application {
+public class W2vFX extends Application {
   private Stage stage;
 
   public static void main(String[] args) {
@@ -18,7 +18,7 @@ public class W2vProject extends Application {
 
   public void start(Stage stage) throws Exception {
     this.stage = stage;
-    stage.setTitle("W2vProject");
+    stage.setTitle("W2vProject ~ for JavaFX ~");
     FScene fs = new FScene(this);
     stage.setScene(fs.getScene());
     stage.show();
